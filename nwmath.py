122e2e36486b1c2bf993d6a8f1d4d3ee694187a2
yanxiaona <872817057@qq.com>
2017-10-31 19:43:28 +0800

    add code

---
+++ nwmath.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+#coding=utf-8
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(numerator, denominator):
+    return float(numerator) / denominator
